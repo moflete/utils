10bf28090d3163c17337a2f23a47604cb5f539db
moflete <116649507+moflete@users.noreply.github.com>
2024-04-11 20:04:39 +0200

    Update create_new_cmake_project.py

--- project_creators/create_new_cmake_project.py
+++ project_creators/create_new_cmake_project.py
@@ -12,9 +12,9 @@ def create_project_structure(project_name):
 
     # Write CMakeLists.txt files
     with open(os.path.join(project_dir, 'CMakeLists.txt'), 'w') as f:
-        f.write('''cmake_minimum_required(VERSION 3.10)
+        f.write('''cmake_minimum_required(VERSION 3.24)
 project({})
-        
+
 # Include directories
 include_directories(include)
 
@@ -22,10 +22,16 @@ include_directories(include)
 add_subdirectory(src)
 '''.format(project_name))
 
+    with open(os.path.join(project_dir, 'src/CMakeLists.txt'), 'w') as f:
+        f.write('''# Include module directories
+add_subdirectory(module1)
+add_subdirectory(module2)
+''')
+
     with open(os.path.join(project_dir, 'src/module1/CMakeLists.txt'), 'w') as f:
         f.write('''# Define module1 library
 add_library(module1
-    src/module1.cpp
+    src/module1.c
 )
 
 # Include directories
@@ -37,7 +43,7 @@ target_include_directories(module1 PUBLIC
     with open(os.path.join(project_dir, 'src/module2/CMakeLists.txt'), 'w') as f:
         f.write('''# Define module2 library
 add_library(module2
-    src/module2.cpp
+    src/module2.c
 )
 
 # Include directories
@@ -52,14 +58,14 @@ target_include_directories(module2 PUBLIC
     with open(os.path.join(project_dir, 'include/common.h'), 'w') as f:
         f.write('// common.h content')
 
-    with open(os.path.join(project_dir, 'src/main.cpp'), 'w') as f:
-        f.write('// main.cpp content')
+    with open(os.path.join(project_dir, 'src/main.c'), 'w') as f:
+        f.write('// main.c content')
 
     with open(os.path.join(project_dir, 'test/CMakeLists.txt'), 'w') as f:
         f.write('''# Set the source files for tests
 set(TEST_SOURCES
-    module1_test.cpp
-    module2_test.cpp
+    module1_test.c
+    module2_test.c
 )
 
 # Create a test executable
@@ -73,11 +79,44 @@ enable_testing()
 add_test(NAME {}_tests COMMAND {}_tests)
 '''.format(project_name, project_name, project_name, project_name))
 
-    with open(os.path.join(project_dir, 'test/module1_test.cpp'), 'w') as f:
-        f.write('// module1_test.cpp content')
+    with open(os.path.join(project_dir, 'test/module1_test.c'), 'w') as f:
+        f.write('// module1_test.c content')
 
-    with open(os.path.join(project_dir, 'test/module2_test.cpp'), 'w') as f:
-        f.write('// module2_test.cpp content')
+    with open(os.path.join(project_dir, 'test/module2_test.c'), 'w') as f:
+        f.write('// module2_test.c content')
+
+    # Write module source files
+    with open(os.path.join(project_dir, 'src/module1/src/module1.c'), 'w') as f:
+        f.write('// module1.c content')
+
+    with open(os.path.join(project_dir, 'src/module2/src/module2.c'), 'w') as f:
+        f.write('// module2.c content')
+
+    # Write module header files
+    with open(os.path.join(project_dir, 'src/module1/include/module1.h'), 'w') as f:
+        f.write('// module1.h content')
+
+    with open(os.path.join(project_dir, 'src/module2/include/module2.h'), 'w') as f:
+        f.write('// module2.h content')
+
+    # Write conanfile.py
+    with open(os.path.join(project_dir, 'conanfile.py'), 'w') as f:
+        f.write('''from conan import ConanFile
+from conan.tools.cmake import cmake_layout
+
+class MyProjectConan(ConanFile):
+    name = "{}"
+    version = "1.0"
+    settings = "os", "compiler", "build_type", "arch"
+    requires = []
+    generators = "CMakeDeps", "CMakeToolchain"
+
+    def requirements(self):
+        self.requires("unity/2.6.0")
+
+    def layout(self):
+        cmake_layout(self)
+'''.format(project_name))
 
 if __name__ == "__main__":
     project_name = input("Enter the project name: ")
